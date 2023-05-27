dce84d376cb20a88fbabf1720fe6bfad0f2cf2d9
Mady93 <popamariana1.pm@gmail.com>
2023-05-27 16:30:14 +0200

    aggiunto address - FormArray e country - FormGroup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +8,8 @@ import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms'
 })
 export class AppComponent {
 
-  title = 'ReactiveForm';
+  title = 'Reactive Form';
+
 
   contactForm: FormGroup;
 
@@ -16,27 +17,26 @@ export class AppComponent {
   lastnameControl: FormControl;
   emailControl: FormControl;
   genderControl: FormControl;
-  isMarriedControl: FormControl;
 
-  /* constructor() {
 
-    this.contactForm = new FormGroup({
+  countryList: any[] = [
+    { id: 1, name: 'Italy' },
+    { id: 2, name: 'USA' },
+    { id: 3, name: 'England' }
+  ];
 
-      firstname: new FormControl(''),
-      lastname: new FormControl(''),
-      email: new FormControl(''),
-      gender: new FormControl(''),
-      isMarried: new FormControl(false)
-    });
-  } */
+  address: FormArray;
 
+
+  hideRemoveButton = true;
+  showSubmitButton = false;
 
   constructor(private formBuilder: FormBuilder) {
 
     this.contactForm = this.formBuilder.group({
 
       firstname: [
-        null,
+        '',
         [
           Validators.minLength(2),
           Validators.pattern('^[a-zA-Z]+$'),
@@ -63,30 +63,158 @@ export class AppComponent {
 
       gender: ['', Validators.required],
 
-      isMarried: [false, Validators.required],
+
+
+
+
+      country: this.formBuilder.group({
+        selectedCountry: ['',
+          [
+            Validators.required
+          ]
+        ]
+      }),
+
+
+
+
+      address: this.formBuilder.array([this.createAddress()], [Validators.required, Validators.minLength(1)])
+
     });
 
 
-    //this.firstnameControl = this.contactForm.get('firstname') as FormControl;
-    this.firstnameControl = <FormControl>this.contactForm.get('firstname');
 
+    this.firstnameControl = <FormControl>this.contactForm.get('firstname');
     this.lastnameControl = <FormControl>this.contactForm.get('lastname');
     this.emailControl = <FormControl>this.contactForm.get('email');
     this.genderControl = <FormControl>this.contactForm.get('gender');
-    this.isMarriedControl = <FormControl>this.contactForm.get('isMarried');
 
+
+    this.address = this.contactForm.get('address') as FormArray;
 
   }
 
+  get countryFormGroup() {
+    return this.contactForm.get('country') as FormGroup;
+  }
+
+  get selectedCountryControl() {
+    return this.countryFormGroup.get('selectedCountry');
+  }
+
+
+
+
+
+  createAddress() {
+    return this.formBuilder.group({
+      city: ['',
+        [
+          Validators.minLength(2),
+          Validators.pattern('^[^0-9]*$'),
+          Validators.required
+        ]
+      ],
+      street: ['', [
+
+        Validators.minLength(2),
+        Validators.pattern('^[^0-9]*$'),
+        Validators.required
+      ]
+      ],
+      zipCode: ['', [
+        Validators.minLength(4),
+        Validators.pattern('^[0-9]*$'),
+        Validators.required
+      ]
+      ]
+
+    })
+  }
+
+  addAddress() {
+    this.address.push(this.createAddress());
+    this.showSubmitButton = this.address.controls.length > 0;
+  }
+
+  removeAddress() {
+    this.address.removeAt(this.address.controls.length - 1);
+  }
 
   onSubmit() {
 
-    alert(this.contactForm.invalid);
 
-    if (this.contactForm.valid) {
-      alert(this.contactForm.valid);
-      return console.log(this.contactForm.value);
+    if (this.contactForm.invalid) {
+      alert('Form is invalid');
+    } else {
+      alert('Form is valid');
+      console.log(this.contactForm.value);
+      this.contactForm.reset();
     }
-    
   }
-} 
+
+
+
+
+
+
+
+
+  // logica css per il bottone submit
+  calculateAngle(e: MouseEvent, item: HTMLElement, parent: HTMLElement): void {
+    let dropShadowColor = `rgba(0, 0, 0, 0.3)`;
+    if (parent.getAttribute('data-filter-color') !== null) {
+      dropShadowColor = parent.getAttribute('data-filter-color')!;
+    }
+
+    const rect = item.getBoundingClientRect();
+    const x = Math.abs(rect.x - e.clientX);
+    const y = Math.abs(rect.y - e.clientY);
+
+    const halfWidth = rect.width / 2;
+    const halfHeight = rect.height / 2;
+
+    const calcAngleX = (x - halfWidth) / 6;
+    const calcAngleY = (y - halfHeight) / 4;
+
+    item.style.transform = `rotateY(${calcAngleX}deg) rotateX(${calcAngleY}deg) scale(1.15)`;
+
+    parent.style.perspective = `${halfWidth * 2}px`;
+    item.style.perspective = `${halfWidth * 3}px`;
+
+    if (parent.getAttribute('data-custom-perspective') !== null) {
+      parent.style.perspective = parent.getAttribute('data-custom-perspective')!;
+    }
+
+    const calcShadowX = (x - halfWidth) / 3;
+    const calcShadowY = (y - halfHeight) / 3;
+
+    item.style.filter = `drop-shadow(${-calcShadowX}px ${calcShadowY}px 15px ${dropShadowColor})`;
+  }
+
+  ngAfterViewInit(): void {
+    const buttons = document.querySelectorAll('.button');
+
+    buttons.forEach((item: HTMLElement) => {
+      item.addEventListener('mouseenter', (e: MouseEvent) => {
+        this.calculateAngle(e, item.querySelector('span') as HTMLElement, item);
+      });
+
+      item.addEventListener('mousemove', (e: MouseEvent) => {
+        this.calculateAngle(e, item.querySelector('span') as HTMLElement, item);
+      });
+
+      item.addEventListener('mouseleave', () => {
+        let dropShadowColor = `rgba(0, 0, 0, 0.3)`;
+        if (item.getAttribute('data-filter-color') !== null) {
+          dropShadowColor = item.getAttribute('data-filter-color')!;
+        }
+        const span = item.querySelector('span') as HTMLElement;
+        span.style.transform = `rotateY(0deg) rotateX(0deg) scale(1)`;
+        span.style.filter = `drop-shadow(0 5px 10px ${dropShadowColor})`;
+      });
+    });
+  }
+
+}
+
